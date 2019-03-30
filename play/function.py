17cc325b583bf00c08689565c953bed13cea6c99
Cynthia Akpene Adjorlolo <cndyly@gmail.com>
2019-03-30 02:01:36 -1200

    my django girls app first commit

---
+++ play/function.py
@@ -0,0 +1,13 @@
+def hi(name):
+	if name == "mary":
+		print ("hi mary")
+	elif name== "becca":
+		print ("hello becca")
+	elif name=="cynthia":
+		print("holla cynthia")
+	else:
+		print("this is your coach")
+
+coders=["mary", "becca", "cynthia", "irene"]
+for coder in coders:
+	hi(coder)
---
+++ play/hello.py
@@ -0,0 +1,15 @@
+volume =90
+if volume < 20:
+	print ("it is kind of white")
+elif 20<= volume <40:
+	print ("it is good for background music")
+elif 40<= volume < 60:
+	print ("perfect i can hear all the details")
+elif 60<= volume < 80:
+	print ("nice for parties")
+elif 80<= volume <100:
+	print ("a bit loud")
+else:
+	print("my ears are hurting")
+
+#this is my own code
